e355ad77c1e78c8c72216f7e6758808a063409ee
Travis Vocino <travis@vocino.com>
2014-02-06 11:57:21 -0800

    bourbon, neat, bitters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 NewedenIo::Application.routes.draw do
-  devise_for :users
+  devise_for :users,  path: '', path_names: { sign_up: 'signup', sign_in: 'signin', sign_out: 'signout' }
 
   get 'dashboard/index'
   root 'dashboard#index'
